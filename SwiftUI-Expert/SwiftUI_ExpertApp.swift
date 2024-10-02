446cb9ede4d3557e01e46ee47f40faeb7e37ad23
ShobhkarTiwari <st.shubh.tiwari@gmail.com>
2024-10-01 22:57:31 -0500

    Initial Commit

---
+++ SwiftUI-Expert/SwiftUI_ExpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExpertApp.swift
+//  SwiftUI-Expert
+//
+//  Created by Shobhakar Tiwari on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-ExpertTests/SwiftUI_ExpertTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExpertTests.swift
+//  SwiftUI-ExpertTests
+//
+//  Created by Shobhakar Tiwari on 10/1/24.
+//
+
+import Testing
+@testable import SwiftUI_Expert
+
+struct SwiftUI_ExpertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

963be5beccd0a4bef0a09b432c97d15464fc8bdc
ShobhkarTiwari <st.shubh.tiwari@gmail.com>
2024-10-04 00:38:17 -0500

    Image bootcamp added

---
+++ SwiftUI-Expert/ImagesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ImagesBootcamp.swift
+//  SwiftUI-Expert
+//
+//  Created by Shobhakar Tiwari on 10/4/24.
+//
+
+import SwiftUI
+
+struct ImagesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagesBootcamp()
+}

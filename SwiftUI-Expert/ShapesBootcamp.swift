d3786820ece74813832d139087effc1bbe7d38f9
ShobhkarTiwari <st.shubh.tiwari@gmail.com>
2024-10-04 00:18:45 -0500

    added shapes bootcamp

---
+++ SwiftUI-Expert/ShapesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftUI-Expert
+//
+//  Created by Shobhakar Tiwari on 10/4/24.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapesBootcamp()
+}

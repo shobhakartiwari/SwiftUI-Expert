ebaf6a1cab238716896e9b93fcf15001aea4174a
ShobhkarTiwari <st.shubh.tiwari@gmail.com>
2024-10-04 00:43:58 -0500

    frame bootcamp added

---
+++ SwiftUI-Expert/FrameBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  FrameBootcamp.swift
+//  SwiftUI-Expert
+//
+//  Created by Shobhakar Tiwari on 10/4/24.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameBootcamp()
+}
